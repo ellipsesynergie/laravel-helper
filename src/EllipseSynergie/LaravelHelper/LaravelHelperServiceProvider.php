92fa6a24a1a16821a9ffef5c0c2880c6c29fcd2e
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2013-09-23 20:53:50 +0400

    Updated service provider

--- src/EllipseSynergie/LaravelHelper/LaravelHelperServiceProvider.php
+++ src/EllipseSynergie/LaravelHelper/LaravelHelperServiceProvider.php
@@ -36,21 +36,61 @@ class LaravelHelperServiceProvider extends ServiceProvider {
 		//Load package config
 		$this->app['config']->package('ellipsesynergie/laravel-helper', __DIR__.'/../../config');
 		
-		//Create the permissions object in the application instance		
-		$this->app['awss3'] = new AwsS3 (
-			Config::get('laravel-helper::aws.s3.key'),
-			Config::get('laravel-helper::aws.s3.secret'),
-			Config::get('laravel-helper::aws.s3.bucket')
-		);
-		
+		//Register helpers
+		$this->registerAssets();
+		$this->registerAjax();
+		$this->registerAwsS3();
+		$this->registerStr();		
+	}
+	
+	/**
+	 * Assets
+	 */
+	public function registerAssets()
+	{
+		//Create the Assets object
+		$this->app['assets'] = new Assets(Config::get('laravel-helper::assets'), url() . DIRECTORY_SEPARATOR, Config::get('laravel-helper::assets.basePath'));
+	}
+
+	/**
+	 * Ajax
+	 */
+	public function registerAjax()
+	{
 		//Create the Ajax object
 		$this->app['ajax'] = new Ajax();
-		
+	}
+	
+	/**
+	 * AwsS3
+	 */	
+	public function registerAwsS3()
+	{
+		//Create the permissions object in the application instance
+		$this->app['awss3'] = new AwsS3 (
+				Config::get('laravel-helper::aws.s3.key'),
+				Config::get('laravel-helper::aws.s3.secret'),
+				Config::get('laravel-helper::aws.s3.bucket')
+		);
+	}
+	
+	/**
+	 * Str
+	 */	
+	public function registerStr()
+	{
 		//Create the Str object
 		$this->app['str'] = new Str(Config::get('application.encoding'));
 		
-		//Create the Assets object
-		$this->app['assets'] = new Assets(Config::get('laravel-helper::assets'), url() . DIRECTORY_SEPARATOR, Config::get('laravel-helper::assets.basePath'));
+		\Str::macro('entities', function($value)
+		{
+			return $this->app['str']->entities($value);
+		});
+		
+		\Str::macro('replaceAccents', function($value)
+		{
+			return $this->app['str']->replaceAccents($value);
+		});
 	}
 
 	/**
