8f3db209e5cb0d49afae3cf46a37c4d0e2965047
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2013-09-25 22:46:49 +0400

    Fixed unit test

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -12,12 +12,6 @@ class StrTest extends \PHPUnit_Framework_TestCase
 		$this->string = new Str('UTF-8');
 	}
 	
-	public function testEntities()
-	{
-		$result = $this->string->entities('<div>test</div>');
-		$this->assertEquals('&lt;div&gt;test&lt;/div&gt;', $result);
-	}
-	
 	public function testReplaceAccent()
 	{
 		$result = $this->string->replaceAccents('Élémentâle');
